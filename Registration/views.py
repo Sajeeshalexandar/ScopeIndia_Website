a716e835fe5769a2a897786ccb913f01eb559d2a
Sajeesh Alexandar S A <alexandersajeesh@gmail.com>
2026-09-10 16:49:28 +0530

    successfully saved the details in the database and showed success and error messages

--- Registration/views.py
+++ Registration/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import StudentRegistrationForm
 
 # Create your views here.
 def registration(request):
-    form = StudentRegistrationForm()
+    if request.method == "POST":
+        form = StudentRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'pages/registration.html',{'success':'Registration Successfull'})
+        else:
+            return render(request,'pages/registration.html',{'error':'Invalid detials Try again!'})
+    else:
+        form = StudentRegistrationForm()
     return render(request,'pages/registration.html',{'form':form})
